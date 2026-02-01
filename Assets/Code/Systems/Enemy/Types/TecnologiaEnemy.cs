4da59c609e635e23176de9f913fa07cdb7a5aa8a
Esteban Abanto <esteban.abanto.2709@gmail.com>
2026-02-01 15:40:39 -0500

    Add ExitoEnemy and TecnologiaEnemy classes with appearance and attack routines

---
+++ Assets/Code/Systems/Enemy/Types/TecnologiaEnemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using PrimeTween;
+using UnityEngine;
+
+public class TecnologiaEnemy : EnemyBase {
+    protected override void OnAppear() {
+        StartCoroutine(AppearRoutine());
+    }
+
+    private IEnumerator AppearRoutine() {
+        transform.position = new Vector3(-9f, 0f, 5f);
+        Tween.Position(transform, new Vector3(0f, 0f, 5f), 1);
+        yield return new WaitForSeconds(2f);
+        if(correctMask) {
+            OnMaskSuccess();
+        } else  {
+            OnMaskFailed();
+        }
+    }
+
+    protected override void OnMaskSuccess() {
+        StartCoroutine(LeaveRoutine());
+    }
+
+    protected override void OnMaskFailed() {
+        StartCoroutine(AttackRoutine());
+    }
+
+    private IEnumerator LeaveRoutine() {
+        Tween.Position(transform, new Vector3(9f, 0f, 5f), 1);
+        yield return new WaitForSeconds(1.5f);
+        Finish();
+    }
+
+    private IEnumerator AttackRoutine() {
+        Tween.Position(transform, new Vector3(0f, 0f, 0f), 1);
+        yield return new WaitForSeconds(3f);
+        Finish();
+    }
+}
